dfa4cc98d2c19227c7ba96700185374fd14adb65
manzing <manzing@gmail.com>
2021-10-09 09:26:27 +0200

    Update Download_EN.bat
    Added instructions for multiples external subs files.

--- Download_EN.bat
+++ Download_EN.bat
@@ -6,7 +6,7 @@ echo ###########################################################################
 echo                               SCRIPT WEB-DL
 echo #############################################################################
 echo *
-echo *      NOTE : Output video will be using the same filename as the input json file   
+echo *      NOTE : Output video will use the same filename as the json input file   
 echo *              
 echo *****************************************************************************
 echo *                !! json file MUST be in script directory !!
@@ -14,11 +14,15 @@ echo ***************************************************************************
 echo *
 set /p key="> Type the name of json file (with extension) :"
 echo *
-echo ** To use external sub, sub file must be a SRT and must have the same filename as json file **
+echo ** To use external sub (max 2), sub files must be a SRT and must have the same filename as json file + "-FR" or "-EN"  **
+echo **
+echo ** Subtitles files must be in script directory, like json file.
+echo **
+echo ** Default languages are FR and EN. Eg: video.json / video-FR.srt / video-EN.srt
 echo ** 
-echo > Use external sub ? (.srt file only) ?
+echo * Use external sub ? (.srt file only) ?
 echo *
-set /p es="> Enter 1 to load a srt file, or type 0 :"
+set /p es="> Type "1" to load srt file(s), or type "0" and press "Enter" :"
 
 if %es% ==1 (goto :ex_sub) else (goto :no_ex_sub)
 
